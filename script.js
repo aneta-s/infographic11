63d40401e896ba027422ba550d77acd7dc9f9f6b
aneta-s <54153719+aneta-s@users.noreply.github.com>
2020-11-24 23:35:29 +0100

    no chart

--- script.js
+++ script.js
@@ -1,23 +1,66 @@
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+//Main chart
+let myChart = document.getElementById('myChart').getContext('2d');
+        
+//Global Options
+Chart.defaults.global.defaultFontFamily= 'Raleway';
+Chart.defaults.global.defaultFontSize= 18;
+Chart.defaults.global.defaultFontColor= 'rgba(68, 67, 92)';
+let massPopChart = new Chart (myChart, {
+            type: 'radar',
+            data: {
+                labels: ['creativiteit', 'inzicht', 'inspireren en overtuigen', 'vooruitstrevend', 'vastbesloten en gepassioneerd', 'altruistisch'],
+                datasets: [{
+                    label: 'Punten',
+                    data: [90, 60, 40, 29, 79, 55
+                    ],
+                    backgroundColor: ['rgba(240, 221, 241)'],
+                    borderWidth:1,
+                    borderColor:'rgba(68, 67, 92)',
+                    hoverBorderWidth:3,
+                    hoverBorderColor:'rgba(68, 67, 92)'
 
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Task', 'Hours per Day'],
-  ['Work', 8],
-  ['Friends', 2],
-  ['Eat', 2],
-  ['TV', 2],
-  ['Gym', 2],
-  ['Sleep', 8]
-]);
+                }]
+            },
+            //Targeting different sections adding Title
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'My Average Day', 'width':550, 'height':400};
+            options: {
+                legend: {
+                    display: false
+                   
+                }
+            },
+            
+        });
+        // Beroepen LBO
+        let myChart2 = document.getElementById('myChart2').getContext('2d');
+        
+//Global Options
+Chart.defaults.global.defaultFontFamily= 'Raleway';
+Chart.defaults.global.defaultFontSize= 18;
+Chart.defaults.global.defaultFontColor= 'rgba(68, 67, 92)';
+let massPopChart = new Chart (myChart, {
+            type: 'bar',
+            data: {
+                labels: ['creativiteit', 'inzicht', 'inspireren en overtuigen', 'vooruitstrevend', 'vastbesloten en gepassioneerd', 'altruistisch'],
+                datasets: [{
+                    label: 'Punten',
+                    data: [90, 60, 40, 29, 79, 55
+                    ],
+                    backgroundColor: ['rgba(240, 221, 241)'],
+                    borderWidth:1,
+                    borderColor:'rgba(68, 67, 92)',
+                    hoverBorderWidth:3,
+                    hoverBorderColor:'rgba(68, 67, 92)'
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}ß
+                }]
+            },
+            //Targeting different sections adding Title
+
+            options: {
+                legend: {
+                    display: false
+                   
+                }
+            },
+            
+        });
